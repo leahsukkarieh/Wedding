78c00499e8ede33f02ccb48a4f9481194abdae11
user.name <lks08@mail.aub.edu>
2024-06-11 18:25:37 +0300

    initial template

---
+++ startup-nextjs-main/components/Footer/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <>
+      <footer
+        className="wow fadeInUp dark:bg-gray-dark relative z-10 bg-white pt-16 md:pt-20 lg:pt-24"
+        data-wow-delay=".1s"
+      >
+        <div className="container">
+          <div className="h-px w-full bg-gradient-to-r from-transparent via-[#D2D8E183] to-transparent dark:via-[#959CB183]"></div>
+          <div className="py-8">
+            <p className="text-center text-base text-body-color dark:text-white">
+              Template by UIdeck and{" "}
+              <a href="https://nextjstemplates.com" rel="nofollow noopener">
+                Next.js Templates
+              </a>
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ startup-nextjs-main/components/Header/menuData.tsx
@@ -0,0 +1,23 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Our Story",
+    path: "",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Get Directions",
+    path: "",
+    newTab: false,
+  },
+];
+export default menuData;
